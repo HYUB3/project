657fcbe61fa01f729b3b0a63c77e68bb91d488a4
Huichan <zgmlcks53@gmail.com>
2022-05-16 11:21:21 +0900

    get file path

--- README.md
+++
@@ -1,5 +0,0 @@
-This projekt is a data analysis tool for the semiconductor industry. It is a tool that can be used for quality control. 
-
-All script is written in Pyhton
-
-The data is not acessable because it's under closure
--- main.py
+++ main.py
@@ -1,71 +1,70 @@
-
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+import os
 import xml.etree.ElementTree as ET
 global wafer_id
 # get input a certain wafer
 wafer_list = ['D07','D08','D23','D24']
+requested_list = []
+final_list = []
 while True:
     wafer_id = input('wafer_id : ')
     if wafer_id in wafer_list:
-        print(wafer_id)
+        requested_list.append(wafer_id)
         break
     else:
         if wafer_id == '':
             for wafer_id in wafer_list:
-                print(wafer_id)
+                requested_list.append(wafer_id)
             break
         else:
             print("This is not a file")
 
-# get the data structure
-tree = ET.parse(wafer_id)
-root = tree.getroot()
-values = []
-for child in root.find('./ElectroOpticalMeasurements/ModulatorSite/Modulator/PortCombo/IVMeasurement'):
-    values.append(child.text)
+# define the relative path of the wafer
+for i in requested_list:
+    os.chdir("../")
+    path = os.getcwd()
+    subpath = (path + "\\HY202103\\" + wafer_id)
+    file_list = os.listdir(subpath)
+    for i in file_list:
+        real_path = subpath + '\\' + i
+        file_list2 = os.listdir(real_path)
+        final_list.append(file_list2)
+    print(final_list)
 
-# load the iv measurement
-voltage_string = values[0].split(',')
-voltage = []
-for i in voltage_string:
-    voltage.append(float(i))
 
-current_string = values[1].split(',')
-current = []
-for i in current_string:
-    current.append(float(i))
 
 # get the data structure
-import xml.etree.ElementTree as ET
-tree = ET.parse(wafer_id)
-root = tree.getroot()
-values = []
-for child in root.find('./ElectroOpticalMeasurements/ModulatorSite/Modulator/PortCombo/IVMeasurement'):
-    values.append(child.text)
-
-# load the iv measurement
-voltage_string = values[0].split(',')
-voltage = []
-for i in voltage_string:
-    voltage.append(float(i))
-
-current_string = values[1].split(',')
-current = []
-for i in current_string:
-    current.append(float(i))
+# tree = ET.parse(wafer_id)
+# root = tree.getroot()
+# values = []
+# for child in root.find('./ElectroOpticalMeasurements/ModulatorSite/Modulator/PortCombo/IVMeasurement'):
+#     values.append(child.text)
+#
+# # load the iv measurement
+# voltage_string = values[0].split(',')
+# voltage = []
+# for i in voltage_string:
+#     voltage.append(float(i))
+#
+# current_string = values[1].split(',')
+# current = []
+# for i in current_string:
+#     current.append(float(i))
+#
+# # get the data structure
+# import xml.etree.ElementTree as ET
+# tree = ET.parse(wafer_id)
+# root = tree.getroot()
+# values = []
+# for child in root.find('./ElectroOpticalMeasurements/ModulatorSite/Modulator/PortCombo/IVMeasurement'):
+#     values.append(child.text)
+#
+# # load the iv measurement
+# voltage_string = values[0].split(',')
+# voltage = []
+# for i in voltage_string:
+#     voltage.append(float(i))
+#
+# current_string = values[1].split(',')
+# current = []
+# for i in current_string:
+#     current.append(float(i))
