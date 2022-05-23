5ff3299b9dae744744b50769101f8c3ad9471ae1
Chaeyoon <chaeyoon20@hanyang.ac.kr>
2022-05-24 02:25:02 +0900

    update gui

--- src/gui.py
+++ src/gui.py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import QIcon
 # from . import process
 
 
-class MainWindow(QMainWindow): #class 자식(부모)
+class MainWindow(QWidget): #class 자식(부모)
     def __init__(self):                 #초기화 함수
         super().__init__()
         # 부모 클래스의 초기화 메소드 호출
@@ -16,7 +16,7 @@ class MainWindow(QMainWindow): #class 자식(부모)
         self.setWindowTitle("Project B3") #name on title bar :이름 추천받아욤
         self.setGeometry(100, 100, 300, 400)
         # ↑ 창의 위치를 모니터 좌상단으로부터 (가로,세로, 창의크기 가로,창의크기 세로)
-        self.setWindowIcon(QIcon('B3.png'))
+        self.setWindowIcon(QIcon('../B3.png'))
         # ↑ 창의 상단바에 아이콘 추가
 
 
@@ -28,85 +28,98 @@ class MainWindow(QMainWindow): #class 자식(부모)
         self.square_label.resize(230,80)
 
 
-        self.label = QLabel("Wafer  ", self)  #waferlabel
-        self.label.move(50, 35)
-        self.label.resize(150, 20)
-        font = self.label.font()
+        self.wafer_label = QLabel("Wafer  ", self)  #waferlabel
+        self.wafer_label.move(50, 35)
+        self.wafer_label.resize(150, 20)
+        font = self.wafer_label.font()              #changing font style
         font.setPointSize(10)
         font.setFamily('Consolas')
         font.setBold(True)
-        self.label.setFont(font)
+        self.wafer_label.setFont(font)
 
-
-        self.label2 = QLabel("Coordinate  ", self)
-        self.label2.move(50, 70)
-        self.label2.resize(150, 20)
-        font2 = self.label2.font()
+        #coordinate label
+        self.coordinate_label = QLabel("Coordinate  ", self)
+        self.coordinate_label.move(50, 70)
+        self.coordinate_label.resize(150, 20)
+        font2 = self.coordinate_label.font()
         font2.setPointSize(10)
         font2.setFamily('Consolas')
         font2.setBold(True)
-        self.label2.setFont(font2)
+        self.coordinate_label.setFont(font2)
 
-        self.waferEdit = QLineEdit("All", self)
-        self.waferEdit.move(170, 35)
-        self.waferEdit.setStyleSheet("color: white;"
+        #Input wafer
+        self.Input_Wafer = QLineEdit("All", self)
+        self.Input_Wafer.move(170, 35)
+        self.Input_Wafer.setStyleSheet("color: white;"
                               "background-color: #002266;"
                               "border-style: solid;"
                               "border-width: 2px;"
-                              "border-radius: 3px;"
-                              "border-color: #white")
-        self.waferEdit.move(170, 35)
-        self.waferEdit.resize(80, 20)
+                              "border-radius: 5px;"
+                              "border-color: #002266")
+        self.Input_Wafer.move(170, 35)
+        self.Input_Wafer.resize(80, 20)
 
-        self.columnEdit = QLineEdit("All", self)
-        self.columnEdit.setStyleSheet("color: white;"
+        #input coordinate
+        self.Input_coordinate = QLineEdit("All", self)
+        self.Input_coordinate.setStyleSheet("color: white;"
                               "background-color: #002266;"
                               "border-style: solid;"
                               "border-width: 2px;"
-                              "border-radius: 3px;"
-                              "border-color: #white")
-        self.columnEdit.move(170, 70)
-        self.columnEdit.resize(80, 20)
+                              "border-radius: 5px;"
+                              "border-color: #002266")
+        self.Input_coordinate.move(170, 70)
+        self.Input_coordinate.resize(80, 20)
 
+
+    #Checkbox
+        #show checkbox
         self.showEdit = QCheckBox("Show", self)
-        self.showEdit.move(100, 110)
+        self.showEdit.move(100, 120)
         self.showEdit.toggle()
 
+        # SaveFigure checkbox
         self.saveEdit = QCheckBox("Save Figure", self)
-        self.saveEdit.move(100, 140)
+        self.saveEdit.move(100, 150)
         self.saveEdit.toggle()
 
+        # Save CSV checkbox
         self.csvEdit = QCheckBox("Save CSV", self)
-        self.csvEdit.move(100, 170)
+        self.csvEdit.move(100, 180)
         self.csvEdit.toggle()
 
         self.label3 = QLabel('', self)
         self.label3.move(50, 170)
 
+
+
+    #button
+        #Set Data Folder Button
         self.btnOpenFolder = QPushButton("Set Data Folder", self) #openfolder button
         self.btnOpenFolder.setStyleSheet("color: white;"
                               "background-color: #002266;"
                               "border-style: solid;"
                               "border-width: 2px;"
-                              "border-radius: 3px;"
+                              "border-radius: 5px;"
                               "border-color: #white")
         self.btnOpenFolder.resize(150, 45) #button size 150*30
         self.btnOpenFolder.move(75, 220) # button location
         self.btnOpenFolder.clicked.connect(self.find_folder) #'clicked' signal이 find_folder 메소드에 연결
 
 
+        # Open Result Folder Button
         self.btnOpenSave = QPushButton("Open Result Folder", self)
         self.btnOpenSave.setStyleSheet("color: white;"
                               "background-color: #002266;"
                               "border-style: solid;"
                               "border-width: 2px;"
-                              "border-radius: 3px;"
+                              "border-radius: 5px;"
                               "border-color: #white")
         self.btnOpenSave.resize(150, 45)
         self.btnOpenSave.move(75, 270)
         self.btnOpenSave.clicked.connect(self.open_folder)
 
 
+        #OK Button
         self.btnSave = QPushButton("OK", self)
         self.btnSave.setStyleSheet("color: white;"
                       "background-color: #002266;"
@@ -135,8 +148,8 @@ class MainWindow(QMainWindow): #class 자식(부모)
         process.open() #수정해야될 부분
 
     def btnInput_clicked(self):
-        wafer = self.waferEdit.text()
-        column = self.columnEdit.text()
+        wafer = self.Input_Wafer.text()
+        column = self.Input_coordinate.text()
         save = self.saveEdit.isChecked()
         show = self.showEdit.isChecked()
         csv = self.csvEdit.isChecked()
