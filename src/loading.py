3d439501e6cab3e866a6ea7430f45a33aa1bb54e
Huichan <zgmlcks53@gmail.com>
2022-05-29 17:57:23 +0900

    loading

--- src/loading.py
+++ src/loading.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 wafer_list = ['D07', 'D08', 'D23', 'D24']
 requested_list = []
@@ -7,7 +8,7 @@ global name_list
 name_list = []
 global wafer_coordinate
 wafer_coordinate = 0
-
+filterlist = []
 
 def data_reader(name_list):
     path = str(os.getcwd()).replace("src", "")
@@ -15,15 +16,39 @@ def data_reader(name_list):
         wafer_id = input('wafer_id : ')
         wafer_coordinate = input('wafer_coordinate : ')
         if wafer_id in wafer_list:
+            listin_folder = os.listdir(f'{path}' + '//data//HY202103//' + f'{wafer_id}')
             requested_list.append(wafer_id)
+            if wafer_coordinate == '':
+                for numb in listin_folder:
+                    try:
+                        list_folder = os.listdir(f'{path}' + '//data//HY202103//' + f'{wafer_id}/{numb}')
+                        for filt in list_folder:
+                            if 'LMZ' in filt:
+                                filterlist.append(filt)
+                    except NotADirectoryError:
+                        break
+            print(len(filterlist))
             break
         else:
             if wafer_id == '':
                 for wafer_id in wafer_list:
                     requested_list.append(wafer_id)
+                    listin_folder = os.listdir(f'{path}' + '//data//HY202103//' + f'{wafer_id}')
+                    if wafer_coordinate == '':
+                        for numb in listin_folder:
+                            try:
+                                list_folder = os.listdir(f'{path}' + '//data//HY202103//' + f'{wafer_id}/{numb}')
+                                for filt in list_folder:
+                                    if 'LMZ' in filt:
+                                        filterlist.append(filt)
+                            except NotADirectoryError:
+                                break
+                print(len(filterlist))
                 break
+
             else:
                 print("This is not a file")
+
 
 
     for wafer in requested_list:
